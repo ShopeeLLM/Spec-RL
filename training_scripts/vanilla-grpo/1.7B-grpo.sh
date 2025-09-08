138acb0607662b9b73561a1083d76a79786e4fa5
bingshuailiu <1187291748@qq.com>
2025-09-08 17:06:44 +0800

    upload training scripts

---
+++ training_scripts/vanilla-grpo/1.7B-grpo.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+PROJECT_DIR=path-to-root-project-dir
+
+cd ${PROJECT_DIR}
+export WORKING_DIR=${PROJECT_DIR}
+
+
+bash ${PROJECT_DIR}/training_scripts/train_grpo.sh \
+    --dataset_name deepmath \
+    --train_file_name train_sample_6k_context_4k \
+    --model_name Qwen3-1.7B-base \
+    --max_response_length 4096 \
+    --train_batch_size 1024 \
+    --rollout_n 8 \
+    --rollout_gpu_memory_util 0.8 \
+    --rollout_tp 2 \
+    --rollout_name vllm \
+    --save_freq 5 \
+    --project_name your-project-name
